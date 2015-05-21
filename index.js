c68429afcdce4d69d6b631fb1ac68b160c4102e8
Marco <marco@hiveempire.com>
2015-05-21 19:55:37 +1000

    master Added lights API (kinda)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var raining = require('./weather/raining.js');
+var ligths = require('./lights/index.js');
+var Repeat = require('repeat');
 
-raining.location('Sydney').then(function (result) {
-  if (result === true) {
-    console.log('raining');
-  } else {
-    console.log('not raining :)');
-  }
-});
+function run() {
+  raining.location('Perth').then(function (result) {
+    if (result === true) {
+      console.log('raining');
+      ligths.setLights('rain');
+    } else {
+      console.log('not raining :)');
+      ligths.setLights('clear');
+    }
+  });
+}
+
+Repeat(run).every(10, 's').for(2, 'minutes').start.in(1, 'sec');
